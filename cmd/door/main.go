85c296853aeda045188099e559765987008c4845
RafaelOliveira <ra.pha@live.com>
2022-10-10 19:43:00 -0300

    Add door service to docker

--- cmd/door/main.go
+++ cmd/door/main.go
@@ -4,23 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/fulecorafa/IoT_server/pkg/db"
+	"github.com/fulecorafa/IoT_server/pkg/log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"io"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"github.com/fulecorafa/IoT_server/pkg/db"
-	"github.com/fulecorafa/IoT_server/pkg/log"
 )
 
-type RainEntry struct {
-	HumidityLevel float64   `json:"humidityLevel"`
-	EntryDate     time.Time `json:"entryDate"`
+type DoorEntry struct {
+	Distance  float64   `json:"distance"`
+	EntryDate time.Time `json:"entryDate"`
 }
 
 // MongoDB connection
@@ -29,11 +27,11 @@ var collection *mongo.Collection
 
 func main() {
 	/* Initializing logger */
-	log.Init("rain")
+	log.Init("door")
 
 	/* Starting microservice */
-	log.Info("Starting rain microservice...")
-	defer log.Info("Rain microservice stopped!")
+	log.Info("Starting door microservice...")
+	defer log.Info("Door microservice stopped!")
 
 	/* Connecting to MongoDB */
 	log.Info("Connecting to MongoDB...")
@@ -41,7 +39,7 @@ func main() {
 	if mongoHostname == "" {
 		log.Fatal("MONGO_HOSTNAME environment variable not set")
 	}
-	client, collection = db.ConnectMongoDB(mongoHostname, "rain")
+	client, collection = db.ConnectMongoDB(mongoHostname, "door")
 	defer log.Info("MongoDB connection closed successfully!")
 	defer func(client *mongo.Client, ctx context.Context) {
 		err := client.Disconnect(ctx)
@@ -54,16 +52,17 @@ func main() {
 
 	/* Starting HTTP server */
 	log.Info("Starting HTTP server...")
-	http.HandleFunc("/", rainHandler)
+	http.HandleFunc("/", doorHandler)
 
-	log.Info("HTTP server up at http://localhost:6969")
-	err := http.ListenAndServe(":6969", nil)
+	log.Info("HTTP server up at http://localhost:6970")
+	err := http.ListenAndServe(":6970", nil)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+
 }
 
-func rainHandler(w http.ResponseWriter, r *http.Request) {
+func doorHandler(w http.ResponseWriter, r *http.Request) {
 	log.LogRequest(r)
 	query := r.URL.Query()
 	switch r.Method {
@@ -94,11 +93,10 @@ func rainHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Error(err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
 		}
 		return
 	case http.MethodPost:
-		var entry RainEntry
+		var entry DoorEntry
 		body, _ := io.ReadAll(r.Body)
 		err := json.Unmarshal(body, &entry)
 		if err != nil {
@@ -106,28 +104,27 @@ func rainHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		fmt.Printf("%v\n", entry)
+		fmt.Println(entry)
 		err = postData(entry)
 		if err != nil {
 			log.Error(err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		isRaining := entry.HumidityLevel > 0.5
+		isOpen := entry.Distance > 10
 		w.WriteHeader(http.StatusCreated)
-		_, err = w.Write([]byte(strconv.FormatBool(isRaining)))
+		_, err = w.Write([]byte(strconv.FormatBool(isOpen)))
 		if err != nil {
 			log.Error(err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
 		}
 		return
 	default:
-		http.Error(w, `{"error": "Method not allowed"}`, http.StatusMethodNotAllowed)
+		http.Error(w, `{"error": "Method not allowed"`, http.StatusMethodNotAllowed)
 	}
 }
 
-func getData(page, limit int) (rainEntries []RainEntry, err error) {
+func getData(page int, limit int) (doorEntries []DoorEntry, err error) {
 	cursor, err := collection.Find(context.TODO(), bson.D{}, options.Find().SetSkip(int64((page-1)*limit)).SetLimit(int64(limit)))
 	if err != nil {
 		return nil, err
@@ -138,15 +135,14 @@ func getData(page, limit int) (rainEntries []RainEntry, err error) {
 			log.Error(err.Error())
 		}
 	}(cursor, context.TODO())
-
-	err = cursor.All(context.TODO(), &rainEntries)
+	err = cursor.All(context.TODO(), &doorEntries)
 	if err != nil {
 		return nil, err
 	}
-	return rainEntries, nil
+	return doorEntries, nil
 }
 
-func postData(entry RainEntry) error {
+func postData(entry DoorEntry) error {
 	_, err := collection.InsertOne(context.TODO(), entry)
 	if err != nil {
 		return err
